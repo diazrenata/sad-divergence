fa65a6c96be58636e45e6b342f2228f20383ec7f
Renata Diaz <diaz.renatam@gmail.com>
2022-10-26 09:42:14 -0400

    resutls

--- scripts/macd_data.R
+++ scripts/macd_data.R
@@ -1,3 +1,6 @@
+library(dplyr)
+source(here::here("functions", "wf.R"))
+
 macd_communities <- read.csv(here::here("data", "macd", "community_analysis_data.csv")) 
 
 macd_sites <- unique(macd_communities$siteID)
@@ -10,8 +13,10 @@ macd_sads <- plyr::compact(macd_sads)
 
 kept_sites <- names(macd_sads)
 
-macd_sads <- macd_sads[1:10]
+#macd_sads <- macd_sads[1:10]
 
 macd_outputs <- lapply(macd_sads, full_workflow)
 
 macd_results <- dplyr::bind_rows(macd_outputs, .id = "siteID")
+
+save.image("macd_output.RData")
